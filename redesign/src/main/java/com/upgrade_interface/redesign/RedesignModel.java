ece3da667c054699dca9c56b9b2c98b791415202
jacob6 <jacob6@gatech.com>
2020-06-13 22:16:07 -0700

    First commit

---
+++ redesign/src/main/java/com/upgrade_interface/redesign/RedesignModel.java
@@ -0,0 +1,13 @@
+package com.upgrade_interface.redesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedesignApplication.class, args);
+	}
+
+}
